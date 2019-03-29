7fd6b91b386f88a823b6fd345e3814fde71dd156
RoderickLabel <roderickruotolo@gmail.com>
2019-03-29 17:22:33 -0300

    Desafio 2 concluido!

--- nodebot-workshop/01-blink-blink.js
+++ nodebot-workshop/01-blink-blink.js
@@ -1,6 +1,9 @@
 var five = require("johnny-five")
 var board = new five.Board()
 
+/**
+ * Pisca o Led disposto no pin 13 a cada 1 segundo
+ */
 board.on("ready", () => {
     var led = new five.Led(13);
     led.blink(1000) 
---
+++ nodebot-workshop/02-servo-wave.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five")
+var board = new five.Board()
+
+/**
+ * Faz movimento de 180 Graus com um Servo ligado no pin 9
+ * durante 3 segundos, para, e retorna ao centro
+ * @see https://github.com/rwaldron/johnny-five/wiki/Servo#api
+ */
+board.on("ready", function() {	
+	
+	servo = new five.Servo(9);	
+
+    servo.sweep({
+        range: [0, 180]
+    });
+
+}).wait(3000, function() {
+    servo.stop();
+    servo.home();
+});
